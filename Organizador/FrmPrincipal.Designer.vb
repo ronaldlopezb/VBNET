87c6f989ed9483d94b04e288374ec96b8e388048
Ronald López <ronaldlopez@ronaldlopezb.com>
2021-05-04 21:06:34 -0500

    solucion de ribon

--- Organizador/FrmPrincipal.Designer.vb
+++ Organizador/FrmPrincipal.Designer.vb
@@ -22,99 +22,13 @@ Partial Class FrmPrincipal
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmPrincipal))
-        Me.RibbonUpDown1 = New System.Windows.Forms.RibbonUpDown()
-        Me.RibbonTab2 = New System.Windows.Forms.RibbonTab()
-        Me.RibbonButton1 = New System.Windows.Forms.RibbonButton()
-        Me.RibbonButton2 = New System.Windows.Forms.RibbonButton()
-        Me.RibbonButton3 = New System.Windows.Forms.RibbonButton()
-        Me.RibbonOrbMenuItem1 = New System.Windows.Forms.RibbonOrbMenuItem()
-        Me.RibbonOrbRecentItem1 = New System.Windows.Forms.RibbonOrbRecentItem()
-        Me.RibbonOrbRecentItem2 = New System.Windows.Forms.RibbonOrbRecentItem()
-        Me.RibbonButton4 = New System.Windows.Forms.RibbonButton()
-        Me.RibbonOrbMenuItem2 = New System.Windows.Forms.RibbonOrbMenuItem()
         Me.PanelMenuPrincipal = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.BtnAbrirCerrar = New System.Windows.Forms.Button()
-        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.PanelMenuPrincipal.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'RibbonUpDown1
-        '
-        Me.RibbonUpDown1.Name = "RibbonUpDown1"
-        Me.RibbonUpDown1.TextBoxText = ""
-        Me.RibbonUpDown1.TextBoxWidth = 50
-        '
-        'RibbonTab2
-        '
-        Me.RibbonTab2.Name = "RibbonTab2"
-        Me.RibbonTab2.Text = "RibbonTab2"
-        '
-        'RibbonButton1
-        '
-        Me.RibbonButton1.Image = CType(resources.GetObject("RibbonButton1.Image"), System.Drawing.Image)
-        Me.RibbonButton1.LargeImage = CType(resources.GetObject("RibbonButton1.LargeImage"), System.Drawing.Image)
-        Me.RibbonButton1.Name = "RibbonButton1"
-        Me.RibbonButton1.SmallImage = CType(resources.GetObject("RibbonButton1.SmallImage"), System.Drawing.Image)
-        '
-        'RibbonButton2
-        '
-        Me.RibbonButton2.Image = CType(resources.GetObject("RibbonButton2.Image"), System.Drawing.Image)
-        Me.RibbonButton2.LargeImage = CType(resources.GetObject("RibbonButton2.LargeImage"), System.Drawing.Image)
-        Me.RibbonButton2.Name = "RibbonButton2"
-        Me.RibbonButton2.SmallImage = CType(resources.GetObject("RibbonButton2.SmallImage"), System.Drawing.Image)
-        '
-        'RibbonButton3
-        '
-        Me.RibbonButton3.Image = CType(resources.GetObject("RibbonButton3.Image"), System.Drawing.Image)
-        Me.RibbonButton3.LargeImage = CType(resources.GetObject("RibbonButton3.LargeImage"), System.Drawing.Image)
-        Me.RibbonButton3.Name = "RibbonButton3"
-        Me.RibbonButton3.SmallImage = CType(resources.GetObject("RibbonButton3.SmallImage"), System.Drawing.Image)
-        '
-        'RibbonOrbMenuItem1
-        '
-        Me.RibbonOrbMenuItem1.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left
-        Me.RibbonOrbMenuItem1.Image = CType(resources.GetObject("RibbonOrbMenuItem1.Image"), System.Drawing.Image)
-        Me.RibbonOrbMenuItem1.LargeImage = CType(resources.GetObject("RibbonOrbMenuItem1.LargeImage"), System.Drawing.Image)
-        Me.RibbonOrbMenuItem1.Name = "RibbonOrbMenuItem1"
-        Me.RibbonOrbMenuItem1.SmallImage = CType(resources.GetObject("RibbonOrbMenuItem1.SmallImage"), System.Drawing.Image)
-        Me.RibbonOrbMenuItem1.Text = "RibbonOrbMenuItem1"
-        '
-        'RibbonOrbRecentItem1
-        '
-        Me.RibbonOrbRecentItem1.Image = CType(resources.GetObject("RibbonOrbRecentItem1.Image"), System.Drawing.Image)
-        Me.RibbonOrbRecentItem1.LargeImage = CType(resources.GetObject("RibbonOrbRecentItem1.LargeImage"), System.Drawing.Image)
-        Me.RibbonOrbRecentItem1.Name = "RibbonOrbRecentItem1"
-        Me.RibbonOrbRecentItem1.SmallImage = CType(resources.GetObject("RibbonOrbRecentItem1.SmallImage"), System.Drawing.Image)
-        Me.RibbonOrbRecentItem1.Text = "RibbonOrbRecentItem1"
-        '
-        'RibbonOrbRecentItem2
-        '
-        Me.RibbonOrbRecentItem2.Image = CType(resources.GetObject("RibbonOrbRecentItem2.Image"), System.Drawing.Image)
-        Me.RibbonOrbRecentItem2.LargeImage = CType(resources.GetObject("RibbonOrbRecentItem2.LargeImage"), System.Drawing.Image)
-        Me.RibbonOrbRecentItem2.Name = "RibbonOrbRecentItem2"
-        Me.RibbonOrbRecentItem2.SmallImage = CType(resources.GetObject("RibbonOrbRecentItem2.SmallImage"), System.Drawing.Image)
-        Me.RibbonOrbRecentItem2.Text = "RibbonOrbRecentItem2"
-        '
-        'RibbonButton4
-        '
-        Me.RibbonButton4.Image = CType(resources.GetObject("RibbonButton4.Image"), System.Drawing.Image)
-        Me.RibbonButton4.LargeImage = CType(resources.GetObject("RibbonButton4.LargeImage"), System.Drawing.Image)
-        Me.RibbonButton4.MaxSizeMode = System.Windows.Forms.RibbonElementSizeMode.Compact
-        Me.RibbonButton4.Name = "RibbonButton4"
-        Me.RibbonButton4.SmallImage = CType(resources.GetObject("RibbonButton4.SmallImage"), System.Drawing.Image)
-        Me.RibbonButton4.Text = "0001"
-        '
-        'RibbonOrbMenuItem2
-        '
-        Me.RibbonOrbMenuItem2.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left
-        Me.RibbonOrbMenuItem2.Image = CType(resources.GetObject("RibbonOrbMenuItem2.Image"), System.Drawing.Image)
-        Me.RibbonOrbMenuItem2.LargeImage = CType(resources.GetObject("RibbonOrbMenuItem2.LargeImage"), System.Drawing.Image)
-        Me.RibbonOrbMenuItem2.Name = "RibbonOrbMenuItem2"
-        Me.RibbonOrbMenuItem2.SmallImage = CType(resources.GetObject("RibbonOrbMenuItem2.SmallImage"), System.Drawing.Image)
-        Me.RibbonOrbMenuItem2.Text = "RibbonOrbMenuItem2"
         '
         'PanelMenuPrincipal
         '
@@ -126,6 +40,15 @@ Partial Class FrmPrincipal
         Me.PanelMenuPrincipal.Size = New System.Drawing.Size(250, 857)
         Me.PanelMenuPrincipal.TabIndex = 1
         '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Info
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(250, 50)
+        Me.Panel1.TabIndex = 5
+        '
         'Panel2
         '
         Me.Panel2.BackColor = System.Drawing.Color.DarkRed
@@ -133,7 +56,7 @@ Partial Class FrmPrincipal
         Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel2.Location = New System.Drawing.Point(250, 0)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(1435, 52)
+        Me.Panel2.Size = New System.Drawing.Size(1370, 52)
         Me.Panel2.TabIndex = 3
         '
         'BtnAbrirCerrar
@@ -145,20 +68,11 @@ Partial Class FrmPrincipal
         Me.BtnAbrirCerrar.Text = "Abrir/Cerrar"
         Me.BtnAbrirCerrar.UseVisualStyleBackColor = True
         '
-        'Panel1
-        '
-        Me.Panel1.BackColor = System.Drawing.SystemColors.Info
-        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(250, 50)
-        Me.Panel1.TabIndex = 5
-        '
         'FrmPrincipal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1685, 857)
+        Me.ClientSize = New System.Drawing.Size(1620, 857)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.PanelMenuPrincipal)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -171,16 +85,6 @@ Partial Class FrmPrincipal
 
     End Sub
 
-    Friend WithEvents RibbonUpDown1 As RibbonUpDown
-    Friend WithEvents RibbonOrbMenuItem1 As RibbonOrbMenuItem
-    Friend WithEvents RibbonOrbRecentItem1 As RibbonOrbRecentItem
-    Friend WithEvents RibbonOrbRecentItem2 As RibbonOrbRecentItem
-    Friend WithEvents RibbonButton4 As RibbonButton
-    Friend WithEvents RibbonTab2 As RibbonTab
-    Friend WithEvents RibbonButton1 As RibbonButton
-    Friend WithEvents RibbonButton2 As RibbonButton
-    Friend WithEvents RibbonButton3 As RibbonButton
-    Friend WithEvents RibbonOrbMenuItem2 As RibbonOrbMenuItem
     Friend WithEvents PanelMenuPrincipal As Panel
     Friend WithEvents Panel2 As Panel
     Friend WithEvents BtnAbrirCerrar As Button
